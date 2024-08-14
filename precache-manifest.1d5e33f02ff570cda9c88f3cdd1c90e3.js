2eec8fea4b3706a7826e47a218ce8a248dd846ba
svdorn <svdorn@wisc.edu>
2024-08-13 17:17:48 -0700

    Updates

---
+++ precache-manifest.1d5e33f02ff570cda9c88f3cdd1c90e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70f844e2b2c2e0e49e2bda83b85b9cd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f80259cac7ce2952b21a",
+    "url": "/static/css/main.2f8235b3.chunk.css"
+  },
+  {
+    "revision": "d5cc672fb8234c8fb8da",
+    "url": "/static/js/2.878021ff.chunk.js"
+  },
+  {
+    "revision": "f80259cac7ce2952b21a",
+    "url": "/static/js/main.25ae4528.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
